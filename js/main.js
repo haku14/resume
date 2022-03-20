9630ff479943d911a3179f10d788b8845023f708
haku14 <hisamutdinovalan39@gmail.com>
2022-03-21 00:35:12 +0300

    Resume
    my resume

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function nav_1(){
+    window.scrollTo({
+        top:900,
+        behavior:"smooth"
+    });
+};
+function nav_3(){
+    window.scrollTo({
+        top:1500,
+        behavior:"smooth"
+    });
+};
