854014ab75bf73b0d9898159def019fba890ec1d
haku14 <hisamutdinovalan39@gmail.com>
2022-04-10 14:35:14 +0300

    Resume
    Resume

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ function nav_1(){
         behavior:"smooth"
     });
 };
+function nav_2(){
+    window.scrollTo({
+        top:4000,
+        behavior:"smooth"
+    });
+};
 function nav_3(){
     window.scrollTo({
         top:4000,
